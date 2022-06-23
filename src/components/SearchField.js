12e5fb7bda3cfd45be7dc5db2446ed362fd9a593
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-23 18:20:58 -0500

    import code to SearchField component

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import { Component } from 'react'
 
-const SearchField = () => {
-  return (
-    <div>SearchField
-        <input>
-            <form>
-                
-            </form>
-        </input>
-    </div>
-  )
+class SearchField extends Component {
+    // onSearchChange = (e) => {
+    //     const searchfield = e.target.value.toLowerCase()
+    //       this.setState(() => {
+    //         return {searchfield}
+    //       })}
+
+  render() {
+        console.log(this.props)
+        const {onSearchChange, placeholder, className} = this.props
+        return (
+            <input 
+                className={className}
+                type="search" 
+                placeholder={placeholder}
+                onChange={onSearchChange} 
+            />
+            
+        )
+    }
 }
+
 
 export default SearchField
