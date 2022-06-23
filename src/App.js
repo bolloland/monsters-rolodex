1da7f2ba48fe6debdd7307b058c4c1f31ff2cfb8
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-23 11:24:58 -0500

    Assign keys to map()

--- src/App.js
+++ src/App.js
@@ -8,15 +8,24 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {first: "Lego", second: "Bricks?"},
-      company: 'RMB'
+      monsters: [
+      {id: 1, name: "Linda"}, 
+      {id: 2, name: "Jack"},
+      {id: 3, name: "Bobby"},
+      {id: 4, name: "Ryan"}
+      ]
     }
   }
 
   render() {
   return (
     <div className="App">
-     
+     {this.state.monsters.map((monst) => {
+          return (
+            <div key={monst.id}>
+              <h1>{monst.name}</h1>
+            </div>)
+        })}
     </div>
   );
 }
