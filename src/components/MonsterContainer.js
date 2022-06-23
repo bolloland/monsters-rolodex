067c7bfb0a8b99f38f4a69de6e2dddb0980968cb
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-23 16:57:22 -0500

    init build monsterContainter

---
+++ src/components/MonsterContainer.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+// import { filterMonsters } from "../App.js" 
+
+class MonsterComponent extends Component {
+
+    render() {
+        console.log(this.props)
+        return (
+        <div>
+        I'm the Monster Component
+      
+        </div>
+        )
+    }
+}
+
+export default MonsterComponent
