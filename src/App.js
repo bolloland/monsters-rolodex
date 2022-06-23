a771b01c995407c44894a2b9a8ead4d0d1f9b183
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-23 16:17:04 -0500

    place searchfield in state to correct backspace

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
 
 //initial State
     this.state = {
-      monsters: []
+      monsters: [], 
+      searchfield: ""
     }
   }
 
@@ -20,36 +21,28 @@ class App extends Component {
     //pull json data from API, and that data will fill the monsters array
     .then((users) => {
       this.setState(
-        () => {
-          return ({monsters: users})
-              }
-          )
-        })
+        () => {return ({monsters: users})}
+          )})
     }
       
-  
 //what to show
   render() {
+    const filterMonsters = this.state.monsters.filter(
+    monst => {return monst.name.toLowerCase().includes(this.state.searchfield)})
+
   return (
     <div className="App">
       <input className="searchbox" type="search" placeholder="search monsters..."
       onChange={(e) => {
-        
-        console.log(e.target.value)
-        const filterMonsters = this.state.monsters.filter(
-          monst => {return monst.name.toLowerCase().includes(e.target.value.toLowerCase()
-          )})
-          
-        this.setState(()=> {
-          return {monsters: filterMonsters}
-        })
-        
-
+        const searchfield = e.target.value.toLowerCase()
+          this.setState(() => {
+            return {searchfield}
+          })
         }}>
         
       </input>
 
-     {this.state.monsters.map((monst) => {
+     {filterMonsters.map((monst) => {
           return (
             <div key={monst.id}>
               <h1>{monst.name} from {monst.address.city}</h1>
