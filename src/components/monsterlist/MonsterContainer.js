0ba8e70f94f256d4bb9d9112258821a73d2827c1
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-24 16:25:55 -0500

    insert MonsterCard compoent in MonContainer

--- src/components/monsterlist/MonsterContainer.js
+++ src/components/monsterlist/MonsterContainer.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
 import './card-list.styles.css'
 import './card-container.styles.css'
+import MonsterCard from './MonsterCard'
+
 
 class MonsterContainer extends Component {
 
@@ -10,16 +12,15 @@ class MonsterContainer extends Component {
         return (
         <div className='card-list'>
           {monsters.map(monster => {
-            const { name, id, email } = monster //destructuring!
             return (
-            <div className="card-container" key={id}>
-            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
-            <h2>{name}</h2>
-            <p>{email}</p>
-          </div>)})}
+              <MonsterCard monster={monster} className="card-container" />
+            )})}
         </div>
         )
     }
 }
 
+
 export default MonsterContainer
+
+      
