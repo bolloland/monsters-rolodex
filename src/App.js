459383d9aaa94b79fc62ec20778aecd841a527bd
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-24 16:24:55 -0500

    add Title to page

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ class App extends Component {
   return (
     <div className="App">
      
+      <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox onSearchChange={onSearchChange} placeholder="search monsters..." className="search-box" />
         <MonsterContainer monsters={filterMonsters} />
 
